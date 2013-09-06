d4f69476afb4e9bbd66420c76b5309dce8f61c64
Sébastien Doeraene <sjrdoeraene@gmail.com>
2013-09-06 16:06:19 +0200

    Add support for blacklisting of tests.

---
+++ partest/src/main/scala/scala/tools/partest/scalajs/ScalaJSPartest.scala
@@ -0,0 +1,94 @@
+/* NSC -- new Scala compiler
+ * Copyright 2005-2013 LAMP/EPFL
+ * @author  Sébastien Doeraene
+ */
+
+package scala.tools.partest
+package scalajs
+
+import nest._
+import Path._
+
+import scala.tools.nsc.{ Global, Settings }
+import scala.tools.nsc.reporters.{ Reporter }
+import scala.tools.nsc.scalajs.JSGlobal
+
+import sbt.testing.{ EventHandler, Logger, Fingerprint }
+import java.io.File
+import java.net.URLClassLoader
+
+trait ScalaJSDirectCompiler extends DirectCompiler {
+  override def newGlobal(settings: Settings, reporter: Reporter): PartestGlobal =
+    new PartestGlobal(settings, reporter) with JSGlobal
+}
+
+trait ScalaJSRunner extends nest.Runner {
+  override def newCompiler = new DirectCompiler(this) with ScalaJSDirectCompiler
+}
+
+trait ScalaJSSuiteRunner extends SuiteRunner {
+  override def runTest(testFile: File): TestState = {
+    // Mostly copy-pasted from SuiteRunner.runTest(), unfortunately :-(
+    val runner = new nest.Runner(testFile, this) with ScalaJSRunner
+
+    // when option "--failed" is provided execute test only if log
+    // is present (which means it failed before)
+    val state =
+      if (failed && !runner.logFile.canRead)
+        runner.genPass()
+      else {
+        val (state, elapsed) =
+          try timed(runner.run())
+          catch {
+            case t: Throwable => throw new RuntimeException(s"Error running $testFile", t)
+          }
+        NestUI.reportTest(state)
+        runner.cleanup()
+        state
+      }
+    onFinishTest(testFile, state)
+  }
+
+  override def runTestsForFiles(kindFiles: Array[File],
+      kind: String): Array[TestState] = {
+    super.runTestsForFiles(kindFiles.filterNot(isBlacklisted), kind)
+  }
+
+  private lazy val blacklistedTestFileNames = {
+    val source = scala.io.Source.fromURL(getClass.getResource(
+        "/scala/tools/partest/scalajs/BlacklistedTests.txt"))
+
+    val srcDir = PathSettings.srcDir
+
+    val blacklistedFileNames = for {
+      line <- source.getLines
+      trimmed = line.trim
+      if trimmed != "" && !trimmed.startsWith("#")
+    } yield {
+      (srcDir / trimmed).toCanonical.getAbsolutePath
+    }
+
+    blacklistedFileNames.toSet
+  }
+
+  def isBlacklisted(testFile: File): Boolean = {
+    blacklistedTestFileNames.contains(testFile.toCanonical.getAbsolutePath)
+  }
+}
+
+/* Pre-mixin ScalaJSSuiteRunner in SBTRunner, because this is looked up
+ * via reflection from the sbt partest interface of Scala.js
+ */
+class ScalaJSSBTRunner(
+    partestFingerprint: Fingerprint,
+    eventHandler: EventHandler,
+    loggers: Array[Logger],
+    srcDir: String,
+    testClassLoader: URLClassLoader,
+    javaCmd: File,
+    javacCmd: File,
+    scalacArgs: Array[String]
+) extends SBTRunner(
+    partestFingerprint, eventHandler, loggers, srcDir, testClassLoader,
+    javaCmd, javacCmd, scalacArgs
+) with ScalaJSSuiteRunner
